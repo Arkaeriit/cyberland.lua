5f5b6e07bb99917563f76afc7d38dc1bbf759806
Arkaeriit <maxbouillot@gmail.com>
2020-04-15 14:13:31 +0200

    When trying to post a picture it scale automatiquely the picture

--- CyberlandPost.lua
+++ CyberlandPost.lua
@@ -2,24 +2,48 @@
 
 local post = {}
 
-post.post = function(board, message, replyTo)
+--make a post and return true or false depending on the succes
+local function send(board, message, replyTo)
     if not replyTo then
         replyTo = 0
     end
-    local cmd = 'curl -d "content='..message..'&replyTo='..replyTo..'" -X POST "https://cyberland2.club/'..board..'/" '
-    os.execute(cmd)
+    local cmd = 'curl -d "content='..message..'&replyTo='..replyTo..'" -X POST "https://cyberland2.club/'..board..'/" --silent'
+    local c = io.popen(cmd,"r")
+    if not c then return false end
+    local ret = c:read("a")
+    return ret == "0"
+end
+
+post.post = function(board, message, replyTo)
+    if send(board, message, replyTo) then
+        io.stdout:write("Message send successfully.\n")
+    else
+        io.stdout:write("Error : message not send.\n")
+    end
 end
 
 -- Generate a picture out of ANSII escpe code from a file
 -- assume that viu is installed and in your $PATH
-local function genPicture(filename)
-    local viu = io.popen("viu '"..filename.."' -w 86","r")
+local function genPicture(filename, size)
+    local viu = io.popen("viu '"..filename.."' -w "..size,"r")
     return viu:read("a")
 end
 
 post.picture = function(board, filename, message, replyTo)
-    local pic = genPicture(filename)
-    post.post(board, pic..message, replyTo)
+    local size = 120
+    local success = false
+    for i=1,110 do
+        local pic = genPicture(filename, size)
+        success = send(board, pic..message, replyTo)
+        if success then
+            io.stdout:write("Message send successfully.\n")
+            break
+        end
+        size = size - 1
+    end
+    if not success then
+        io.stdout:write("Error : message not send.\n")
+    end
 end
 
 return post
